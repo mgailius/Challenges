21de462c8793a6f8bb6312e30fd4fe4bc08d053f
Martynas <mart.gailius@gmail.com>
2018-08-19 09:01:44 +0300

    completed challenge: Find E to Nth Digit

---
+++ JavaScript/Numbers/Find e to the Nth Digit/script.js
@@ -0,0 +1,12 @@
+function setPI() {
+  do {
+    const length = parseInt(prompt('| Find E to the Nth Digit\n| Enter a number between 1 and 50'));
+    if (length <= 50) {
+      const PI = Math.E.toFixed(length);
+      alert(PI);
+      break;
+    } else { alert('Enter a valid number'); }
+  } while (true);
+}
+
+setPI();
